97e31aec46482f8f9bcc8515d87f7782515b5c7c
Ahmad <ataufiq665@gmail.com>
2018-02-21 02:26:37 +0700

    Styling and add drawable background

--- app/src/main/java/makanbu/com/makanbu/MainActivity.java
+++ app/src/main/java/makanbu/com/makanbu/MainActivity.java
@@ -1,13 +1,16 @@
 package makanbu.com.makanbu;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_order);
     }
 }
