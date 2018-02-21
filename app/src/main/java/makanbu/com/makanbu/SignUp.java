4adb309f366c53a09c65f375f794b8ad3052bd50
auginahylda <auginahylda@gmail.com>
2018-02-21 09:50:37 +0700

    add signup layout

---
+++ app/src/main/java/makanbu/com/makanbu/SignUp.java
@@ -0,0 +1,18 @@
+package makanbu.com.makanbu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+
+
+}
