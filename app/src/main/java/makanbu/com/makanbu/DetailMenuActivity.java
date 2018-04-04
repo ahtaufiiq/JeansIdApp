9595f0eeaf827545f0900ec7d496e9343474d711
labbaikatiovani <labbaikaa4@gmail.com>
2018-04-04 09:00:41 +0700

    Add Gambar

--- app/src/main/java/makanbu/com/makanbu/DetailMenuActivity.java
+++ app/src/main/java/makanbu/com/makanbu/DetailMenuActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 /**
  * Created by Labbaika Putri on 2/21/2018.
@@ -11,11 +14,22 @@ import android.view.View;
 
 public class DetailMenuActivity extends AppCompatActivity {
 
+    ImageView gambar_card, profileImage_card;
+    TextView namaMenu_card, jumlahReview_card, hargaMakanan_card;
+    RatingBar rating_card;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_menu);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        gambar_card = findViewById(R.id.gambar_card);
+        profileImage_card = findViewById(R.id.profileImage_card);
+        namaMenu_card = findViewById(R.id.namaMenu_card);
+        jumlahReview_card = findViewById(R.id.jumlahReview_card);
+        hargaMakanan_card = findViewById(R.id.hargaMakanan_card);
+        rating_card = findViewById(R.id.rating_card);
 
 
     }
